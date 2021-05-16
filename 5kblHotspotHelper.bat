a75e171be4a8d157bb26700bcd8d000e9f692667
E-Soda <64815861+E-Soda@users.noreply.github.com>
2021-05-15 19:06:26 -0700

    Automatically Obtain Admin Permission

--- 5kblHotspotHelper.bat
+++ 5kblHotspotHelper.bat
@@ -1,20 +1,7 @@
 @echo off
 
-goto check_Permissions
-
-:check_Permissions
-
-    net session >nul 2>&1
-    if %errorLevel% == 0 (
-        goto setup
-    ) else (
-		echo.
-        echo Please run this script again with admininstrator priviliges.
-		echo.
-	    pause
-		exit
-
-	)
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
+cd /d "%~dp0"
 
 :setup
 setlocal enableDelayedExpansion
